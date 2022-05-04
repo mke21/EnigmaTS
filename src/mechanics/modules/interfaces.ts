5411953dddd998095f69999bb9483177fd3abec9
Marcel <4930603+mke21@users.noreply.github.com>
2022-05-04 23:07:57 +0200

    added plugboard

--- src/mechanics/modules/interfaces.ts
+++ src/mechanics/modules/interfaces.ts
@@ -1,12 +1,16 @@
 const CHECKSTRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
 
+export function validate(value: string): boolean {
+	return (value.length !== 1 || !CHECKSTRING.includes(value)) ;
+}
+
 export class CharacterCarrier {
 	readonly value: string
 	rotate: boolean = false
 
 	constructor(value: string, rotate: boolean=false) {
 		let upperCase = value.toUpperCase();
-		if (value.length !== 1 || !CHECKSTRING.includes(upperCase)) {
+		if (validate(upperCase)) {
 			throw `Value "${value} is not a character that could be typed in enigma`;
 		}
 		this.value = upperCase;
