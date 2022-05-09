31348b8cd188eedea14fcb77f9f6d9e8dc03dea9
Marcel <4930603+mke21@users.noreply.github.com>
2022-05-09 21:51:28 +0200

    bugfix keeping key pressed results in many events

--- src/gui/input/input.ts
+++ src/gui/input/input.ts
@@ -2,15 +2,22 @@ import { EventsBase } from "ev/EventsBaseClass";
 const LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 export class KeyInput extends EventsBase{
+
+  private pressed: boolean = false;
+
   constructor() {
     super(["KeyPress", "KeyRelease"]);
     let that = this;
     window.onkeydown = k => {
-      if (LETTERS.includes(k.key)) {
+      if (LETTERS.includes(k.key) && !that.pressed) {
+        that.pressed = true;
         that._events.KeyPress.notify(k.key.toUpperCase());
       }
     };
 
-    window.onkeyup = () => that._events.KeyRelease.notify("up");
+    window.onkeyup = () => {
+      that.pressed = false;
+      that._events.KeyRelease.notify("up");
+    }
   }
 }
