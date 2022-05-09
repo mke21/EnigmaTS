1285de3dbc6800b13b14d267aac73d558a6f58b7
Marcel <4930603+mke21@users.noreply.github.com>
2022-05-09 21:37:37 +0200

    reset button

--- src/gui/index.ts
+++ src/gui/index.ts
@@ -1,3 +1,4 @@
+import { append } from "helper/html";
 import { Board } from "./lightboard/board";
 import { Machine } from "mechanics/machine";
 import { KeyInput } from "./input/input";
@@ -6,12 +7,17 @@ export class App {
   public board: Board = new Board();
   public machine: Machine = new Machine();
   private input: KeyInput = new KeyInput();
+  private reset: HTMLButtonElement = document.createElement("button");
 
   constructor() {
-    document.body.appendChild(this.board.div);
+    this.reset.classList.add("resetbutton");  
+    this.reset.textContent = "Reset";
+    append(document.body, [this.board.div, this.reset])
+
     this.input.addEventListener("KeyPress", (key: any) => {
       this.board.on(this.machine.encrypt(String(key)));
     });
     this.input.addEventListener("KeyRelease", this.board.off);
+    this.reset.onclick = this.machine.reset;
   }
 }
