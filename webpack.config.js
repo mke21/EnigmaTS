d64d3824975bc21bbe09a83b009a24cacd2648c4
Marcel Kerkveld <m.kerkveld@rijkzwaan.nl>
2022-05-18 13:23:17 +0200

    fixed paths for windows

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "/src/index.ts"),
+  entry: path.resolve(__dirname, "src/index.ts"),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "enigma.js",
