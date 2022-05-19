6c81b8f895dd2ad5dfe93fb6654d4aa876e628c5
Marcel <4930603+mke21@users.noreply.github.com>
2022-05-19 19:05:48 +0200

    simplified wire

--- src/mechanics/modules/plugboard.ts
+++ src/mechanics/modules/plugboard.ts
@@ -2,24 +2,7 @@
 
 import { CharacterCarrier, validate } from "./interfaces";
 
-
-export class Wire {
-  readonly side1: string;
-  readonly side2: string;
-
-  constructor(side1: string, side2: string) {
-    let s1 = side1.toUpperCase();
-    let s2 = side2.toUpperCase();
-    if (!validate(s1) || !validate(s2)) {
-      throw "plugs not correct, should be a single letter";
-    }
-    if (s1 === s2) {
-      throw "cannot plug wire into itself";
-    }
-    this.side1 = s1;
-    this.side2 = s2;
-  }
-}
+interface Wire{side1: string, side2: string};
 
 export class PlugBoard {
   connections = new Map<string, string>();
@@ -31,11 +14,20 @@ export class PlugBoard {
   }
 
   add(wire: Wire): void {
+    let s1 = wire.side1.toUpperCase();
+    let s2 = wire.side2.toUpperCase();
+    // validate
+    if (!validate(s1) || !validate(s2)) {
+      throw "plugs not correct, should be a single letter";
+    }
+    if (s1 === s2) {
+      throw "cannot plug wire into itself";
+    }
     if (this.connections.has(wire.side1) || this.connections.has(wire.side2)) {
       throw "Cannot connect mutiple wires to the same socket";
     }
-    this.connections.set(wire.side1, wire.side2);
-    this.connections.set(wire.side2, wire.side1);
+    this.connections.set(s1, s2);
+    this.connections.set(s2, s1);
   }
 
   delete(wire: Wire) {
